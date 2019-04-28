ac4119d35023427ab4d7eaa29e3626fb87f822aa
zhangzhuo <862340288@qq.com>
2019-04-28 18:33:09 +0800

    1

--- main.ts
+++ main.ts
@@ -457,29 +457,6 @@ namespace robotbit {
         matrixShow();
     }
 
-    //% blockId=robotbit_ultrasonic block="Ultrasonic|pin %pin"
-    //% weight=10
-    export function Ultrasonic(pin: DigitalPin): number {
-
-        // send pulse
-        pins.setPull(pin, PinPullMode.PullNone);
-        pins.digitalWritePin(pin, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(pin, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(pin, 0);
-
-        // read pulse
-        let d = pins.pulseIn(pin, PulseValue.High, 25000);
-        let ret = d;
-        // filter timeout spikes
-        if (ret == 0 && distanceBuf!= 0){
-            ret = distanceBuf;
-        }
-        distanceBuf = d;
-        return Math.floor(ret*10/6/58);
-    }
-
     let pin = DigitalPin.P0;
     function signal_dht11(pin: DigitalPin): void {
         pins.digitalWritePin(pin, 0)
